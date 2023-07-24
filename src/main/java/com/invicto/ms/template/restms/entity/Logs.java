725df3da6812ffd3f9a001d99deb9243404bf0c1
null <null>
2023-07-24 14:59:11 +0000

    json ignore for date

--- src/main/java/com/invicto/ms/template/restms/entity/Logs.java
+++ src/main/java/com/invicto/ms/template/restms/entity/Logs.java
@@ -2,7 +2,7 @@ package com.invicto.ms.template.restms.entity;
 
 import  javax.persistence.*;
 import java.time.LocalDateTime;
-
+import com.fasterxml.jackson.annotation.JsonIgnore;
 @Entity
 @Table(name = "LOGS")
 public class Logs{
@@ -10,6 +10,7 @@ public class Logs{
     @Id
     public String id;
     public String api;
+    @JsonIgnore
     public LocalDateTime localDateTime;
 
 
