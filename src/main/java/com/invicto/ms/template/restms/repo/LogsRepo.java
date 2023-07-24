c3862828bc032667fa473c9f7bab601cc2d8b089
null <null>
2023-07-24 14:45:00 +0000

    repo

--- src/main/java/com/invicto/ms/template/restms/repo/LogsRepo.java
+++ src/main/java/com/invicto/ms/template/restms/repo/LogsRepo.java
@@ -2,6 +2,7 @@ package com.invicto.ms.template.restms.repo;
 
 import org.springframework.data.repository.CrudRepository;
 import com.invicto.ms.template.restms.entity.*;
+import org.springframework.stereotype.Repository;
 
 @Repository
 public interface LogsRepo extends CrudRepository<Logs,String>{
